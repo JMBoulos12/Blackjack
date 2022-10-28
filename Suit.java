868ea850efcbcf4b86a66353366b852c2533c457
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-28 13:40:49 +0300

    Create Suit.java

---
+++ Suit.java
@@ -0,0 +1,12 @@
+/**
+ * An enum represnting the four possible suits of a card.
+ *
+ * @author Jean Marc Boulos
+ * @Since 28/10/2022
+ */
+public enum Suit {
+  Clubs,
+  Diamonds,
+  Spades,
+  Hearts,
+}
